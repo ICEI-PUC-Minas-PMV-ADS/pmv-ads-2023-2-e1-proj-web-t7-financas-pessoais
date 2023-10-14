d4a9f01574b63c71317e433450dffaa45d06d7c2
gustavofbraga <gustavofbraga@gmail.com>
2023-10-14 19:31:07 -0300

    feat: adiciona grafico Doughnut na dashboard

--- codigo-fonte/src/script.js
+++ codigo-fonte/src/script.js
@@ -18,3 +18,42 @@ function carregarMenu() {
 }
 
 carregarMenu();
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Casa', 'Carro', 'Viagem', 'Faculdade'],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [40, 35, 25, 20],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)',
+                'rgb(745, 115, 56)',
+            ],
+            hoverOffset: 4
+        }]
+    },
+    options: {
+        layout: {
+            padding: {
+                bottom: 140,
+                top: 0
+            }
+        },
+        plugins: {
+            legend: {
+                display: true,
+                position: 'right', 
+                labels: {
+                    font: {
+                        size: 20
+                    }
+                }
+            }
+        }
+    }
+});
