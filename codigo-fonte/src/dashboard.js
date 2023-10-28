c8248798886729882bc294d53cc0d73db39ebb44
Luzianne Fialho <lffialho@sga.pucminas.br>
2023-10-28 09:27:29 -0300

    [FIX] Adição do script.js que foi removido indevidamente

--- codigo-fonte/src/dashboard.js
+++ codigo-fonte/src/dashboard.js
@@ -1,24 +1,3 @@
-function carregarMenu(menu = 'menu.html') {
-    const menuContainer = document.getElementById('menu-container');
-    const menuUrl = menu;
-
-    fetch(menuUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao carregar o menu.');
-            }
-            return response.text();
-        })
-        .then(menuHtml => {
-            menuContainer.innerHTML = menuHtml;
-        })
-        .catch(error => {
-            console.error('Erro ao carregar o menu:', error);
-        });
-}
-
-carregarMenu();
-
 // DASHBOARD
 
 //Dados despesas por categorias
